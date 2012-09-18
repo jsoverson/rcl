c167b88254cc857aa8e0767512b6d87660670875
Jarrod Overson <joverson@onerecovery.com>
2012-09-17 23:47:02 -0700

    serving a web client in conjunction

--- tasks/helpers/index.js
+++ tasks/helpers/index.js
@@ -7,11 +7,26 @@ exports.startServer = function(address, port) {
   address = address || '0.0.0.0';
   port = port || 8888;
 
-  var app = require('http').createServer(),
+  var app = require('http').createServer(handler),
       io = require('socket.io').listen(app),
       fs = require('fs');
 
   app.listen(port);
+
+  function handler (req, res) {
+    var file = '/webclient.html';
+    if (req.url === '/socket.js') file = '/socket.io.0.9.10.js';
+    fs.readFile(__dirname + file,
+      function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error loading ' + file);
+        }
+
+        res.writeHead(200);
+        res.end(data);
+      });
+  }
 
   return io;
 
