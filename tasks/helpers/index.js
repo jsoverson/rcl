164c572ae689b3a4e4fdec931d50214a95b428d7
Jarrod Overson <joverson@onerecovery.com>
2012-10-05 15:02:23 -0700

    moved bulk of task to helper

--- tasks/helpers/index.js
+++ tasks/helpers/index.js
@@ -1,5 +1,9 @@
-var grunt = require('grunt'),
-    fs = require('fs');
+var grunt   = require('grunt'),
+    fs      = require('fs'),
+    log4js  = require('log4js');
+    glogger = log4js.getLogger('rcl');
+    format  = require('./string-format');
+;
 
 exports.startServer = function(address, port) {
   "use strict";
@@ -29,3 +33,36 @@ exports.startServer = function(address, port) {
   return io;
 
 };
+
+exports.startRcl = function(port) {
+  var io = exports.startServer(undefined, port),
+      loggers = {};
+
+  io.set('log level',1); // warnings only
+  glogger.debug('Connected on port ' + port);
+
+  io.sockets.on('connection', function (socket) {
+    socket.on('rcl', function (data) {
+      var file = data.caller.file,
+        logger = loggers[file] ? loggers[file] : loggers[file] = log4js.getLogger(file),
+        location = ['l', data.caller.line, ':', data.caller.col].join(''),
+        formats = format.parse(data.args[0]),
+        dataToLog = data.args;
+
+      if (formats.length > 0) {
+        dataToLog = format(data.args.shift(), data.args);
+      } else {
+        for (var i = 0; i < data.args.length; i++) {
+          try {
+            data.args[i] = JSON.parse(data.args[i]);
+          } catch(e) {}
+        }
+      }
+      data.args = dataToLog;
+      logger[data.level].apply(logger,[location].concat(dataToLog));
+      if (data.loopback) socket.emit('client:rcl',data);
+      socket.broadcast.emit('client:rcl', data);
+    });
+  });
+  return io;
+}
